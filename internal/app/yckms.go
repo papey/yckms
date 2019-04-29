511177217343af793980c228d32326ef8ac5ec73
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-04-29 15:30:50 +0200

    Add parsing playlist functions
    Add stuff to extract playlist from podcast description using regex and split

--- internal/app/yckms.go
+++ internal/app/yckms.go
@@ -1,18 +1,105 @@
 package app
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/mmcdole/gofeed"
 )
+
+type song struct {
+	title  string
+	artist string
+}
+type show struct {
+	name     string
+	playlist []song
+}
+
+// parse description and extract playlist
+// Input exemple :
+// 		<p>Au programme :</p>
+// 		<p>- Revue de presse : Matthieu</p>
+// 		<p>- Chronique Fidlar : Théo</p>
+// 		<p>- Chronique Waste Of Space Orchestra : Eline</p>
+// 		<p><br></p>
+// 		<p>Playlist : Bus / I Buried Paul, Nails / Endless Resistance, Sepultura /
+// 		Territory, Venom / Evilution Devilution, All Pigs Must Die / The Whip, Fidlar
+// 		/ Too Real, Obituary / Slowly We Rot, Wayfarer / Catcher, Waste of Space
+// 		Orchestra / Seeker's Reflection, Bat / Long Live the Lewd, Witchfinder /
+// 		Ouija, Gadget /Choice of a Lost Generation</p>
+// First step (1) :
+// 		Bus / I Buried Paul, Nails / Endless Resistance, Sepultura /
+// 		Territory, Venom / Evilution Devilution, All Pigs Must Die / The Whip, Fidlar
+// 		/ Too Real, Obituary / Slowly We Rot, Wayfarer / Catcher, Waste of Space
+// 		Orchestra / Seeker's Reflection, Bat / Long Live the Lewd, Witchfinder /
+// 		Ouija, Gadget /Choice of a Lost Generation
+// Second step (2) :
+// 		An array containing each combo Artist / Song
+// Third step (3) :
+//		A song object
+func parsePlaylist(desc string) ([]song, error) {
+
+	var s []song
+
+	// Split on carriage return
+	split := strings.Split(desc, "\n")
+
+	// pltf is the last element is the playlist, but not formated (1)
+	plnf := split[len(split)-1]
+
+	// remove trailing <p> and </p>
+	// prepare regex
+	reg, err := regexp.Compile(`<p>Playlist : (.+)</p>`)
+	if err != nil {
+		return nil, err
+	}
+
+	// pl contain the string playlist (1)
+	pl := reg.FindSubmatch([]byte(plnf))
+	if pl == nil {
+		return nil, errors.New("No match found in show description")
+	}
+
+	// convert to string
+	list := string(pl[1])
+
+	// Split by ", " (2)
+	songs := strings.Split(list, ", ")
+
+	// for each song
+	for _, e := range songs {
+		elem := strings.Split(e, "/")
+		// TRIM, just to be sure
+		song := song{title: strings.Trim(elem[0], " "), artist: strings.Trim(elem[1], " ")}
+		s = append(s, song)
+		fmt.Printf("Artist : %s, Song : %s\n", song.artist, song.title)
+	}
+
+	return s, err
+
+}
 
 // SyncLast will sync last show
 func SyncLast(url string) error {
 
 	// get show
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(url)
-	fmt.Println(feed.Title)
+	feed, err := fp.ParseURL(url)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	// pass last show as arg, extract songs from playlist
+	songs, err := parsePlaylist(feed.Items[0].Description)
+	if err != nil {
+		return err
+	}
+
+	s := show{name: feed.Title, playlist: songs}
+	fmt.Println(s)
+
+	return err
 }
