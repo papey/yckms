7c3439abe8e5e3d25be4a55a7e3c7a8bee3c3c9a
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-05-05 10:49:33 +0200

    Split package app into multiple files

---
+++ internal/app/parsing.go
@@ -0,0 +1,75 @@
+package app
+
+import (
+	"regexp"
+	"strings"
+)
+
+// parse description and extract playlist
+// Input exemple :
+// 		<p>Au programme :</p>
+// 		<p>- Revue de presse : Matthieu</p>
+// 		<p>- Chronique Fidlar : Théo</p>
+// 		<p>- Chronique Waste Of Space Orchestra : Eline</p>
+// 		<p><br></p>
+// 		<p>Playlist : Bus / I Buried Paul, Nails / Endless Resistance, Sepultura /
+// 		Territory, Venom / Evilution Devilution, All Pigs Must Die / The Whip, Fidlar
+// 		/ Too Real, Obituary / Slowly We Rot, Wayfarer / Catcher, Waste of Space
+// 		Orchestra / Seeker's Reflection, Bat / Long Live the Lewd, Witchfinder /
+// 		Ouija, Gadget /Choice of a Lost Generation</p>
+// First step (1) :
+// 		Bus / I Buried Paul, Nails / Endless Resistance, Sepultura /
+// 		Territory, Venom / Evilution Devilution, All Pigs Must Die / The Whip, Fidlar
+// 		/ Too Real, Obituary / Slowly We Rot, Wayfarer / Catcher, Waste of Space
+// 		Orchestra / Seeker's Reflection, Bat / Long Live the Lewd, Witchfinder /
+// 		Ouija, Gadget /Choice of a Lost Generation
+// Second step (2) :
+// 		An array containing each combo Artist / Song
+// Third step (3) :
+//		A song object
+func parsePlaylist(desc string) ([]song, error) {
+
+	var s []song
+
+	// Split on carriage return
+	split := strings.Split(desc, "\n")
+
+	// pltf is the last element is the playlist, but not formated (1)
+	if len(split) == 0 {
+		return nil, nil
+	}
+
+	plnf := split[len(split)-1]
+
+	// remove trailing <p> and </p>
+	// prepare regex
+	reg, err := regexp.Compile(`(?:Playlist|PLAYLIST|Setlist) : (.+)`)
+	if err != nil {
+		return nil, err
+	}
+
+	// pl contain the string playlist (1)
+	pl := reg.FindSubmatch([]byte(plnf))
+	if pl == nil {
+		return nil, nil
+	}
+
+	// convert to string
+	list := string(pl[1])
+
+	// Split by ", " (2)
+	songs := strings.Split(list, ", ")
+
+	// for each song
+	for _, e := range songs {
+		elem := strings.Split(e, "/")
+		// TRIM, just to be sure
+		if len(elem) >= 2 {
+			song := song{title: strings.Trim(elem[1], " "), artist: strings.Trim(elem[0], " ")}
+			s = append(s, song)
+		}
+	}
+
+	return s, err
+
+}
---
+++ internal/app/playlist.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/zmb3/spotify"
+)
+
+// createPlaylist is used to wrap all playlist things
+func createPlaylist(s *show, user string, client *spotify.Client) error {
+
+	// create playlist
+	pl, err := client.CreatePlaylistForUser(user, s.name, s.desc, true)
+	if err != nil {
+		return err
+	}
+
+	// image setup
+	err = client.SetPlaylistImage(pl.ID, s.image)
+	if err != nil {
+		return err
+	}
+
+	// add songs
+	err = addSongsToPlaylist(s.playlist, pl, client)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Playlist for show '%s' created, see %s\n", s.name, pl.URI)
+
+	return nil
+}
+
+// addSongsToPlaylist loops orver songs from a show and add it to a playlist
+func addSongsToPlaylist(songs []song, pl *spotify.FullPlaylist, client *spotify.Client) error {
+
+	// array of tracks IDs
+	var tracks []spotify.ID
+
+	// for each songs in a show
+	for _, elem := range songs {
+		// forge search query
+		search := fmt.Sprintf("artist:%s track:%s", elem.artist, elem.title)
+
+		// search
+		res, err := client.Search(search, spotify.SearchTypeTrack)
+		if err != nil {
+			return err
+		}
+
+		// search query is specific, so first item is always the best
+		if res.Tracks.Total > 0 {
+			// add to array
+			tracks = append(tracks, res.Tracks.Tracks[0].ID)
+		}
+
+	}
+
+	// Add all tracks to playlist
+	_, err := client.AddTracksToPlaylist(pl.ID, tracks...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
