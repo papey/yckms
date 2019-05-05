9d8aa5d016084da23892e5f53ea31276c07d1ea6
Wilfried OLLIVIER <wollivier@fdn.fr>
2019-05-05 10:59:13 +0200

    Test format

--- internal/app/yckms_test.go
+++ internal/app/yckms_test.go
@@ -6,13 +6,11 @@ import (
 
 func TestCreateImage(t *testing.T) {
 
-
 	url := "https://image.ausha.co/SHSw9XAonLSu3xLyQBpTOT1ai6bpGCw8LKceHuan_1400x1400.jpeg?t=1556571464"
 
 	_, err := createImage(url)
 	if err != nil {
 		t.Error("Error: Can't create image")
 	}
-
 
 }
