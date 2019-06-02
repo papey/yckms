3f8ea8088d2409f257cad143a820f3af19f65130
Wilfried OLLIVIER <wollivier@bearstech.com>
2019-06-02 11:58:20 +0200

    Bump version

--- cmd/yckms.go
+++ cmd/yckms.go
@@ -16,7 +16,7 @@ func main() {
 	// Basic config
 	app.Name = "YCKMS"
 	app.Usage = "Sync playlists from frech metal podcasts shows to Spotify"
-	app.Version = "0.0.5"
+	app.Version = "0.0.5-1"
 
 	// Flags
 	app.Flags = []cli.Flag{
