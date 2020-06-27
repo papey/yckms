2e971e1f69d0dcb96486530c5ab1d3d840b173df
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-06-27 17:12:20 +0200

    From Harry Cover to Recoversion

--- internal/app/parsing.go
+++ internal/app/parsing.go
@@ -279,7 +279,7 @@ func InitParse(name, title, desc string) Parser {
 		return NewYCKM(name, title, desc)
 	case "Le Bruit":
 		return NewLB(name, title, desc)
-	case "Harry Cover, le podcast des meilleures reprises":
+	case "Recoversion (ex Harry Cover), le podcast des meilleures reprises":
 		return NewHC(name, title, desc)
 	case "La Pifothèque":
 		return NewPifo(name, title, desc)
